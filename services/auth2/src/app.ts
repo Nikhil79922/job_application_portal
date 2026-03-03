16b6bef298b8d5f42ab20106cb5771b9e2098479
Nikhil_Singh.dev <139366599+Nikhil79922@users.noreply.github.com>
2026-03-03 07:22:03 +0000

    Added the new Project dir

---
+++ services/auth2/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import authRouter from './routes/auth.js';
+import errorMiddleware from './middleware/errorMiddleware.js';
+import logger from './middleware/logger.js';
+import { connectAdmin } from './library/kafka/admin.js';
+import { KafkaProducer } from './library/kafka/producer.js';
+import { redisClient } from './library/redis/index.js';
+import cookieParser from "cookie-parser";
+ const app =express();
+
+ //global Route Logs logger.
+app.use(logger); 
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+//Kafka Admin and Prodcuer
+connectAdmin()
+KafkaProducer.connect();
+
+//Redis connection
+redisClient.connect().then(()=>{
+    console.log("✅ connected the Redis server")
+}).catch((err)=>{
+    console.log('❌ failed to connect to redis',err)
+})
+
+app.use('/api/auth',authRouter)
+
+app.use(errorMiddleware) //global Error.
+
+
+export default app;
---
+++ services/auth2/src/index.ts
@@ -0,0 +1,35 @@
+import app from './app.js'
+import dotenv from 'dotenv'
+import { sql } from './library/neonDB/db.js';
+import { UserModel } from './models/users/usersTable.js';
+import { SkillsModel } from './models/skills/skillsTable.js';
+import { UserSkillsModel } from './models/user_skills/user_skillsTable.js';
+import { RefreshTokenModel } from './models/refresh_token/refresh_tokenTable.js';
+dotenv.config();
+
+let port = process.env.PORT
+
+//DB
+async function initDB() {
+    try {
+       await UserModel.createRoleEnum();
+
+       await UserModel.createTable(); 
+    
+       await SkillsModel.createTable();
+
+       await UserSkillsModel.createTable();
+
+       await RefreshTokenModel.createTable();
+       
+        console.log("✅ DataBase initialization successfully done",);
+    } catch (e) {
+        console.log("❌ Error in DataBase initialization", e);
+        process.exit(1);
+    }
+}
+initDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Auth Server is Listening at Port ${port}`)
+    })
+})
