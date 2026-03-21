d9ebd3e1209ae4fdee0ee396a9066d6db3d42b12
Nikhil79922 <ns94301918@gmail.com>
2026-03-21 18:33:01 +0530

    Added template

---
+++ services/auth/src/containers/auth/register.container.ts
@@ -0,0 +1,30 @@
+
+// Infra
+import { upload } from "../../infra/storage/fileUpload.js"
+
+// Services
+import { authRegister } from "../../domain/services/auth/register.service.js";
+import { BcryptPasswordService } from "../../infra/security/password.service.js";
+import { JwtTokenService } from "../../infra/security/token.service.js";
+
+// Repositories
+import { PostgresUserRepository } from "../../infra/database/repository/user.repository.js";
+import { RefreshTokenTable } from "../../infra/database/repository/refreshToken.repository.js";
+
+
+const fileUpload = new upload()
+
+const userRepo = new PostgresUserRepository();
+const refreshRepo = new RefreshTokenTable();
+
+const passwordService = new BcryptPasswordService();
+const tokenService = new JwtTokenService();
+// const deviceService = new DeviceService();
+
+export const authRegisterService = new authRegister(
+  userRepo,
+  refreshRepo,
+  passwordService,
+  tokenService,
+  fileUpload
+);
