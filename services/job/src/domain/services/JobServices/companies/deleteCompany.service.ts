9c65c029f5ae2977fe7ea5a93e8dae8dc8b342d8
Nikhil79922 <ns94301918@gmail.com>
2026-04-05 16:10:25 +0530

    Added the clean up

--- services/job/src/domain/services/JobServices/companies/deleteCompany.service.ts
+++ services/job/src/domain/services/JobServices/companies/deleteCompany.service.ts
@@ -2,15 +2,15 @@ import { deleteCompanyDTO } from "../../../../api/dtos/deleteCompany.schema.js";
 import { Users } from "../../../../shared/types/user.type.js";
 import { ICompaniesRepository } from "../../../interfaces/repoInterfaces/companies.repository.interface.js";
 
-export class deleteCompanySer{
+export class deleteCompanySer {
     constructor(
-        private companyRepo :ICompaniesRepository 
-    ){}
+        private companyRepo: ICompaniesRepository
+    ) { }
 
-    async deleteCompany(data:deleteCompanyDTO, userDetails:Users){
-let deleteCount : any= await this.companyRepo.delete(data.companyId,userDetails.user_id);
-if(deleteCount){
-return {message : "Company deleted along with the jobs successfully"};
-} 
-}
+    async deleteCompany(data: deleteCompanyDTO, userDetails: Users) {
+        let deleteCount: any = await this.companyRepo.delete(data.companyId, userDetails.user_id);
+        if (deleteCount) {
+            return { message: "Company deleted along with the jobs successfully" };
+        }
+    }
 }
