9cc195b11bb2d56fd0fb6530c30b2929ac19fe6c
Nikhil79922 <ns94301918@gmail.com>
2026-05-02 17:29:14 +0530

    Added the upload content Kakfa topic with 2 partiton in kafka Admin  config in utils service

--- services/utils/src/infra/messaging/config/kafka.admin.ts
+++ services/utils/src/infra/messaging/config/kafka.admin.ts
@@ -5,6 +5,7 @@ export class KafkaAdmin {
   private admin: Admin | null = null;
   private connecting: Promise<void> | null = null;
   private readonly SEND_MAIL_TOPIC = "send-mail";
+  private readonly UPLOAD_CONTENT_TOPIC = "upload-content";
 
   async connect(): Promise<void> {
     if (this.admin) return;
@@ -14,19 +15,27 @@ export class KafkaAdmin {
         const admin = kafka.admin();
        await admin.connect();
         console.log("✅ Kafka Admin connected");
-        const topics = await admin.listTopics(); 
+        
+        await admin.createTopics({
+          topics: [
+            {
+              topic: this.SEND_MAIL_TOPIC,
+              numPartitions: 1,
+              replicationFactor: 1,
+            },
+            {
+              topic: this.UPLOAD_CONTENT_TOPIC,
+              numPartitions: 2,
+              replicationFactor: 1,
+            },
+          ],
+          waitForLeaders: true,
+        });
 
-        if (!topics.includes(this.SEND_MAIL_TOPIC)) {
-          await admin.createTopics({
-            topics: [
-              {
-                topic: this.SEND_MAIL_TOPIC,
-                numPartitions: 1,
-                replicationFactor: 1,
-              },
-            ],
-          });
-        }
+        console.log("📌 Topics ensured:", [
+          this.SEND_MAIL_TOPIC,
+          this.UPLOAD_CONTENT_TOPIC,
+        ]);
 
         this.admin = admin;
       } catch (error) {
